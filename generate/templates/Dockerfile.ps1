c4bff1ba8c32cf9a5524a5a5a5f6e4ea740b2a27
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-09-26 06:44:03 +0000

    Feature: Add v1.5.3153.0-fh2-4.6.304 variant

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -47,11 +47,59 @@ RUN mv "extract/bf2statisitcs 2.2.0/Server Files/Linux/python" /server/bf2/pytho
 
 "@
 }
+if ('fh2' -in $VARIANT['_metadata']['components']) {
+    @"
+WORKDIR /root
+RUN curl -ssLO http://fhbeta.warumdarum.de/~spitfire/fh2pub/2.63/fh2-server-$( $VARIANT['_metadata']['mod_installer_version'] ).tar
+RUN sha256sum fh2-server-$( $VARIANT['_metadata']['mod_installer_version'] ).tar | grep "$( $VARIANT['_metadata']['mod_installer_sha256sum'] )"
+RUN tar -C /server/bf2 -xvf fh2-server-$( $VARIANT['_metadata']['mod_installer_version'] ).tar
+RUN chown -R root:root /server/bf2
+RUN chmod +x /server/bf2/start-fh2.sh
+RUN chmod -R +x /server/bf2/bin
+RUN mv /server/bf2/start-fh2.sh /server/bf2/start.sh
+RUN for i in `$( ls /server/bf2/mods/fh2/levels/*/info/*cq_64_menumap.png | cut -d '/' -f7 ); do echo "maplist.append \"`$i\" \"gpm_cq\" 64" >> /server/bf2/mods/fh2/settings/maplist.con; done # Create a default maplist.con, since it is not included by the installer
+
+
+"@
+}
+
 
 @"
 FROM ubuntu:16.04 AS final
 COPY --from=install /server /server
 RUN export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y ca-certificates && rm -rf /var/lib/apt/lists/*
+
+"@
+if ('fh2' -in $VARIANT['_metadata']['components']) {
+    @"
+# Install fh2 dependencies
+# Fix error: /usr/lib/x86_64-linux-gnu/libstdc++.so.6: version `GLIBCXX_3.4.22' not found (required by /server/bf2/bin/amd-64/fh2utils.so)
+# See: https://stackoverflow.com/questions/43070900/version-glibcxx-3-4-22-not-found
+RUN apt-get update && apt-get install -y apt-transport-https software-properties-common \
+    && add-apt-repository ppa:ubuntu-toolchain-r/test \
+    && apt-get update \
+    && apt-get install -y gcc-4.9 \
+    && apt-get upgrade -y libstdc++6 \
+    && strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX \
+    && add-apt-repository ppa:ubuntu-toolchain-r/test --remove \
+    && apt-get purge --auto-remove -y apt-transport-https software-properties-common gcc-4.9
+# Fix flash of error on startup:
+#   Could not find platform independent libraries <prefix>.
+#   Could not find platform dependent libraries <exec_prefix>
+#   Consider setting `$PYTHONHOME to <prefix>[:<exec_prefix>]
+#   PlayerManager created
+#   ObjectManager created
+#   GameLogic created
+#   Serversettings created
+ENV PYTHONHOME=
+
+"@
+}
+@"
 WORKDIR /server/bf2
 CMD [ "./start.sh" ]
 "@
+
+
+
+
