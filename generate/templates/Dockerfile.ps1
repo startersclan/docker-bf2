dae208e5e5aa557b1d4a0c1f1c8343c8954432d6
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-10-07 15:03:08 +0000

    Enhancement: Bump `bf2stats-2.3.0` variants to `bf2stats-2.3.1`

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -50,16 +50,16 @@ RUN mv "extract/bf2statisitcs 2.2.0/Server Files/Linux/python" /server/bf2/pytho
 
 "@
 }
-if ('bf2stats-2.3.0' -in $VARIANT['_metadata']['components']) {
+if ('bf2stats-2.3.1' -in $VARIANT['_metadata']['components']) {
     @"
-# Install bf2stats 2.3.0
+# Install bf2stats 2.3.1
 WORKDIR /root
 RUN export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y unzip
-RUN curl -sSLO https://github.com/startersclan/bf2stats/archive/refs/tags/2.3.0.zip
-RUN sha256sum 2.3.0.zip | grep '^4e91c5cdda63aaff1e2ccc20f40befcb603000eac25221be4cefbdebfdee6aec '
-RUN unzip 2.3.0.zip -d extract
+RUN curl -sSLO https://github.com/startersclan/bf2stats/archive/refs/tags/2.3.1.zip
+RUN sha256sum 2.3.1.zip | grep '^b9569819f7c58b70ff3e66d9219aed78ad6433d5cbba58f46b7bc0bf9eeb6d89 '
+RUN unzip 2.3.1.zip -d extract
 RUN rm -rf /server/bf2/python
-RUN mv extract/bf2stats-2.3.0/src/python /server/bf2/python
+RUN mv extract/bf2stats-2.3.1/src/python /server/bf2/python
 
 
 "@
