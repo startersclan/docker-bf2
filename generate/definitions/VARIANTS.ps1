78c769a77d2c88d6828f85af0aa9712a953bb5aa
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-10-24 05:49:21 +0000

    Refactor: Update installer URLs to download from clan site

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -6,7 +6,7 @@ $VARIANTS = @(
             platforms = 'linux/386,linux/amd64'
             components = @()
             installer_version = '1.5.3153.0'
-            installer_sha256sum = 'b5afe6602280b99797b46389f3cd582bbfe212e7402c8b009b72e6ae41e8d435'
+            installer_sha256sum = '4d849218c1725e7bd6a7e7f164e27b036248f8ded2e30340dd0722c1dfffbab6'
         }
         # Docker image tag
         tag = 'v1.5.3153.0'
@@ -18,7 +18,7 @@ $VARIANTS = @(
             platforms = 'linux/386,linux/amd64'
             components = @( 'bf2hub' )
             installer_version = '1.5.3153.0'
-            installer_sha256sum = 'b5afe6602280b99797b46389f3cd582bbfe212e7402c8b009b72e6ae41e8d435'
+            installer_sha256sum = '4d849218c1725e7bd6a7e7f164e27b036248f8ded2e30340dd0722c1dfffbab6'
         }
         # Docker image tag
         tag = 'v1.5.3153.0-bf2hub'
@@ -30,7 +30,7 @@ $VARIANTS = @(
             platforms = 'linux/386,linux/amd64'
             components = @( 'bf2stats-2.3.3' )
             installer_version = '1.5.3153.0'
-            installer_sha256sum = 'b5afe6602280b99797b46389f3cd582bbfe212e7402c8b009b72e6ae41e8d435'
+            installer_sha256sum = '4d849218c1725e7bd6a7e7f164e27b036248f8ded2e30340dd0722c1dfffbab6'
         }
         # Docker image tag
         tag = 'v1.5.3153.0-bf2stats-2.3.3'
@@ -42,7 +42,7 @@ $VARIANTS = @(
             platforms = 'linux/386,linux/amd64'
             components = @( 'bf2stats-3.1.1' )
             installer_version = '1.5.3153.0'
-            installer_sha256sum = 'b5afe6602280b99797b46389f3cd582bbfe212e7402c8b009b72e6ae41e8d435'
+            installer_sha256sum = '4d849218c1725e7bd6a7e7f164e27b036248f8ded2e30340dd0722c1dfffbab6'
         }
         # Docker image tag
         tag = 'v1.5.3153.0-bf2stats-3.1.1'
@@ -54,7 +54,7 @@ $VARIANTS = @(
             platforms = 'linux/amd64' # fh2 can't get c++6 dependencies for linux/386, so we only build linux/amd64
             components = @( 'fh2' )
             installer_version = '1.5.3153.0'
-            installer_sha256sum = 'b5afe6602280b99797b46389f3cd582bbfe212e7402c8b009b72e6ae41e8d435'
+            installer_sha256sum = '4d849218c1725e7bd6a7e7f164e27b036248f8ded2e30340dd0722c1dfffbab6'
             mod_installer_version = '4.6.304'
             mod_installer_sha256sum = 'bb933052ad20928b5a4bc6c1eeff647d62b0f3b38de46d063101719a9f0cf488'
         }
